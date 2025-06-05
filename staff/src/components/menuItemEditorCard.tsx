d629dc6c03ad8958a5097c3aa82bbbf2c7497604
sota <sota.matsui@akane.waseda.jp>
2025-06-05 17:43:09 +0900

    feat: メニューアイテムエディタの画像アップロード機能などUIコンポーネントを改善

--- staff/src/components/menuItemEditorCard.tsx
+++ staff/src/components/menuItemEditorCard.tsx
@@ -2,6 +2,20 @@
 import { Prisma } from '@prisma/client';
 import { ActionDispatch } from 'react';
 import { ReducerAction } from '@/components/menuItemEditor';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@radix-ui/react-label';
+import { FileImageIcon, Trash2 } from 'lucide-react';
+import { Switch } from '@/components/ui/switch';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Textarea } from './ui/textarea';
 export function MenuItemEditorCard(props: {
   menuItem: Prisma.MenuItemCreateInput,
   index: number,
@@ -10,20 +24,54 @@ export function MenuItemEditorCard(props: {
   const { menuItem, index, dispatch } = props;
 
   return (
-    <div className='flex flex-col'>
-      <div className='flex justify-between bg-foreground/10 rounded-tl-md rounded-tr-md px-2 py-1.5 text-sm'>
-        {menuItem.name}
-        <button
-          onClick={() => {
-            dispatch({ type: 'remove', index: index });
-          }}
-          className='text-red-500 hover:text-red-700'
-        >
-          削除
-        </button>
+    <div style={{ '--image-url': `url(${menuItem.imageUrl})` } as React.CSSProperties}
+      className={'flex flex-col max-w-sm text-background bg-linear-to-b bg-[image:linear-gradient(var(--tw-gradient-stops)),_var(--image-url)] from-black/50 to-black/50 bg-cover rounded-md'}>
+      <div className='flex justify-end items-center px-2 py-1.5'>
+        <div>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant='ghost'>
+                <FileImageIcon className='size-4' />
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>商品画像の設定</DialogTitle>
+                <DialogDescription>
+                  メニューに表示される商品画像を設定します。画像のアップロードは、保存ボタンを押したタイミングで行われます。
+                </DialogDescription>
+              </DialogHeader>
+              <input type='file' onChange={(e) => {
+                if (e.target.files && e.target.files.length > 0) {
+                  const file = e.target.files[0];
+                  dispatch({
+                    type: 'uploadImage',
+                    index: index,
+                    image: file
+                  });
+                }
+              }}
+                className="file:mr-4 file:rounded-full file:border-0 file:bg-violet-50 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-violet-700 hover:file:bg-violet-100 dark:file:bg-violet-600 dark:file:text-violet-100 dark:hover:file:bg-violet-500 ..."
+              />
+            </DialogContent>
+          </Dialog>
+          <Button
+            onClick={() => {
+              dispatch({ type: 'remove', index: index });
+            }}
+            variant='ghost'
+          >
+            <Trash2 className='size-4 text-red-500' />
+          </Button>
+        </div>
       </div>
-      <div className='bg-foreground/5 rounded-bl-md rounded-br-md p-2'>
-        <input
+      <div className='p-2'>
+        <Label htmlFor='name' className='text-sm mb-1'>商品名</Label>
+        <Input
+          id='name'
+          name='name'
+          placeholder='商品名'
+          autoComplete='off'
           type="text"
           value={menuItem.name}
           onChange={(e) => {
@@ -35,10 +83,11 @@ export function MenuItemEditorCard(props: {
               }
             })
           }}
-          className='w-full mb-2 p-1 border border-gray-300 rounded'
         />
-        <textarea
+        <Label htmlFor='description' className='text-sm mb-1'>商品説明</Label>
+        <Textarea
           value={menuItem.description || ''}
+          className='inline-block field-sizing-fixed'
           onChange={(e) => {
             dispatch({
               type: 'update',
@@ -48,51 +97,46 @@ export function MenuItemEditorCard(props: {
               }
             })
           }}
-          className='w-full mb-2 p-1 border border-gray-300 rounded'
         />
-        <input
-          type="number"
-          value={menuItem.price}
-          onChange={(e) => {
-            dispatch({
-              type: 'update',
-              index: index,
-              menuItem: {
-                price: parseInt(e.target.value)
-              }
-            })
-          }}
-          className='w-full mb-2 p-1 border border-gray-300 rounded'
-        />
-        <p className='block mb-2'>
-          <label htmlFor='isAvailable' className='text-sm'>注文可能</label>
-          <input
-            type="checkbox"
-            checked={menuItem.isAvailable}
-            id='isAvailable'
-            name='isAvailable'
+        <Label htmlFor='price' className='text-sm mb-1'>価格</Label>
+        <div className='flex items-center gap-1'>
+          <span>¥</span>
+          <Input
+            id='price'
+            name='price'
+            placeholder='価格'
+            autoComplete='off'
+            type="number"
+            value={menuItem.price}
             onChange={(e) => {
               dispatch({
                 type: 'update',
                 index: index,
                 menuItem: {
-                  isAvailable: e.target.checked
+                  price: parseInt(e.target.value)
                 }
               })
             }}
-            className='ml-2'
+            className='grow'
           />
-        </p>
-        <input type='file' onChange={(e) => {
-          if (e.target.files && e.target.files.length > 0) {
-            const file = e.target.files[0];
-            dispatch({
-              type: 'uploadImage',
-              index: index,
-              image: file
-            });
-          }
-        }} />
+        </div>
+        <div className='flex justify-between items-center pt-2'>
+          <label htmlFor='isAvailable' className='text-sm'>注文を一時停止</label>
+          <Switch
+            checked={!(menuItem.isAvailable)}
+            onCheckedChange={(checked) => {
+              dispatch({
+                type: 'update',
+                index: index,
+                menuItem: {
+                  isAvailable: !checked
+                }
+              })
+            }}
+            id='isAvailable'
+            className='mr-2'
+          />
+        </div>
       </div>
     </div>
   );
