d2d1c63c3aaae97eeee65a9d65730dccec2ad3c0
sota <sota.matsui@akane.waseda.jp>
2025-06-06 14:22:37 +0900

    feat: メニュー一覧ページにヘッダーを追加し、カートポップアップのUIを改善

--- customer/src/components/cartPopup.tsx
+++ customer/src/components/cartPopup.tsx
@@ -1,42 +1,55 @@
 'use client';
 import { useCartStore } from "@/providers/cart-store-provider";
-import Link from "next/link";
 import { useState } from "react";
+import { ChevronDownIcon, ChevronUpIcon, Minus, Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export function CartPopup() {
   const { currentCart, totalPrice, addItem, removeItem } = useCartStore((state) => state);
   const [isHidden, setIsHidden] = useState(false);
+  const router = useRouter()
   return (
-    <div className="fixed z-20 bottom-0 w-full bg-background shadow-lg rounded-lg px-4 max-h-dvh overflow-y-auto">
-      <div className="flex justify-between items-center py-2 sticky top-0 bg-background">
-        <h2 className="" onClick={() => setIsHidden(!isHidden)}>ご注文 {isHidden ? '🔽' : '🔼'}</h2>
-        <p className="">計￥{totalPrice.toString()}</p>
-        <Link href='/checkout'>
-          <button
-            className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-400 disabled:bg-foreground/20" disabled={currentCart.length === 0}>
-            注文する
-          </button>
-        </Link>
-      </div>
-      <div className={isHidden ? 'hidden' : 'block'}>{currentCart.map((entry) => (
-        <div key={entry.menuItemId}>
-          <p className="text-lg">{entry.name}</p>
-          <p className="text-sm text-gray-600">￥{entry.price.toString()}</p>
-          <p className="text-sm text-gray-600">数量: {entry.quantity}</p>
-          <button
-            className="m-2 bg-foreground/10 px-4 py-2 rounded hover:bg-foreground/5"
-            onClick={() => addItem(entry.menuItem)}
-          >
-            +
-          </button>
-          <button
-            className="m-2 bg-foreground/10 px-4 py-2 rounded hover:bg-foreground/5"
-            onClick={() => removeItem(entry.menuItem)}
-          >
-            -
-          </button>
+    <div className="fixed z-20 bottom-0 w-full p-2">
+      <div className="w-full bg-background drop-shadow-lg rounded-lg px-4 max-h-dvh overflow-y-auto">
+        <div className="flex justify-between items-center py-2 sticky top-0 bg-background">
+          <Button variant='ghost' onClick={() => setIsHidden(!isHidden)}>
+            <p>ご注文リスト</p>
+            {isHidden ? <ChevronDownIcon className="size-5" /> : <ChevronUpIcon className="size-5" />}
+          </Button>
+          <div className="flex items-center gap-2">
+            <p className="">計￥{totalPrice.toLocaleString()}</p>
+            <Button disabled={currentCart.length === 0} onClick={() => router.push('/checkout')}>
+              注文
+            </Button>
+          </div>
         </div>
-      ))}</div>
+        <div className={`transition-[height] duration-300 ${isHidden ? 'h-[50vh]' : 'h-0 overflow-hidden'}`}>
+          {currentCart.map((entry) => (
+            <div key={entry.menuItemId} className="border-b py-2">
+              <p className="text-lg">{entry.name}</p>
+              <div className="flex justify-between items-center">
+                <p className="text-sm text-gray-600">￥{entry.price.toLocaleString()}</p>
+                <div className="flex items-center gap-2">
+                  <p className="text-sm text-gray-600">数量: {entry.quantity}</p>
+                  <Button
+                    variant="outline"
+                    onClick={() => addItem(entry.menuItem)}
+                  >
+                    <Plus className="size-4" />
+                  </Button>
+                  <Button
+                    variant="outline"
+                    onClick={() => removeItem(entry.menuItem)}
+                  >
+                    <Minus className="size-4" />
+                  </Button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
